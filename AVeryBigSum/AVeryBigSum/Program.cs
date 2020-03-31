6384360a10d3633190fb06313ae9c6841bdca589
Leandro Alves Santos <lealsto82@gmail.com>
2020-03-31 20:57:08 -0300

    A Very Big Sum solution

---
+++ AVeryBigSum/AVeryBigSum/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVeryBigSum
+{
+    class Program
+    {
+        // Complete the aVeryBigSum function below.
+        static long aVeryBigSum(long[] ar)
+        {
+
+            long result = 0;
+
+            for (int i = 0; i < ar.Count(); ++i)
+            {
+                result += ar[i];
+            }
+
+            return result;
+
+        }
+
+        static void Main(string[] args)
+        {
+            
+
+            int arCount = Convert.ToInt32(Console.ReadLine());
+
+            long[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt64(arTemp));
+            long result = aVeryBigSum(ar);
+
+
+            //To run on Visual Studio
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+            /*
+            //To run on HackerRank
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+            */
+        }
+    }
+}
