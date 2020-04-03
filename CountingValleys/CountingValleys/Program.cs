8878bd3d4e87f6e25c1d1a8ff5ae50d9421ed1b9
Leandro Alves Santos <lealsto82@gmail.com>
2020-04-03 02:39:57 -0300

    Counting Valleys Solutions

---
+++ CountingValleys/CountingValleys/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingValleys
+{
+    class Program
+    {
+        // Complete the countingValleys function below.
+        static int countingValleys(int n, string s)
+        {
+
+            int valleysTraversed = 0;
+            int level = 0;  //0 is the sea level
+
+            for (int i = 0; i < s.Length; ++i)
+            {
+                if (s[i] == 68)
+                {
+                    if (level == 0)
+                        ++valleysTraversed;
+
+                    --level;
+                }
+                else if (s[i] == 85)
+                    ++level;
+            }
+
+            return valleysTraversed;
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            string s = Console.ReadLine();
+
+            int result = countingValleys(n, s);
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+            /*
+            //To run on hackerRank
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+            */
+        }
+    }
+}
