5c1e9f35aba6361ead66d13542099c0b79069e65
Leandro Alves Santos <lealsto82@gmail.com>
2020-04-04 04:28:50 -0300

    Find Digits solution

---
+++ FindDigits/FindDigits/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindDigits
+{
+    class Program
+    {
+        // Complete the findDigits function below.
+        static int findDigits(int n)
+        {
+            int divisorToFindDigit = 1;
+            int digit;
+            int numOfDivisors = 0;
+
+            while (divisorToFindDigit < n)
+            {
+                digit = (n / divisorToFindDigit) % 10; //Based on a formula given at discussions page
+                divisorToFindDigit *= 10;
+
+                if (digit == 0)
+                    continue;
+
+                if (n % digit == 0)
+                    numOfDivisors++;
+            }
+
+            return numOfDivisors;
+
+        }
+
+        static void Main(string[] args)
+        {
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                int result = findDigits(n);
+
+                //textWriter.WriteLine(result);
+
+                Console.WriteLine(result);
+            }
+
+            //textWriter.Flush();
+            //textWriter.Close();
+
+            Console.ReadKey();
+        }
+
+    }
+}
