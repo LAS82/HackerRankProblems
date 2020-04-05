808966cc20d25cf30cc091c6b72f886c6629d907
Leandro Alves Santos <lealsto82@gmail.com>
2020-04-04 21:46:03 -0300

    Cats and Mouse solution

--- Cat and Mouse/Cat and Mouse/Program.cs
+++ Cat and Mouse/Cat and Mouse/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,47 @@ namespace Cat_and_Mouse
 {
     class Program
     {
+        // Complete the catAndMouse function below.
+        static string catAndMouse(int x, int y, int z)
+        {
+
+            int distanceToCatA = Math.Abs(z - x);
+            int distanceToCatB = Math.Abs(z - y);
+
+            if (distanceToCatA < distanceToCatB)
+                return "Cat A";
+            else if (distanceToCatA > distanceToCatB)
+                return "Cat B";
+            else
+                return "Mouse C";
+
+        }
+
         static void Main(string[] args)
         {
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int q = Convert.ToInt32(Console.ReadLine());
+
+            for (int qItr = 0; qItr < q; qItr++)
+            {
+                string[] xyz = Console.ReadLine().Split(' ');
+
+                int x = Convert.ToInt32(xyz[0]);
+
+                int y = Convert.ToInt32(xyz[1]);
+
+                int z = Convert.ToInt32(xyz[2]);
+
+                string result = catAndMouse(x, y, z);
+
+                //textWriter.WriteLine(result);
+                Console.WriteLine(result);
+            }
+
+            //textWriter.Flush();
+            //textWriter.Close();
+            Console.ReadKey();
         }
     }
 }
