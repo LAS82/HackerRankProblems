9f8b24ded0d651af213ad2d6b2deaf014ac6a4bd
Leandro Alves Santos <lealsto82@gmail.com>
2020-04-05 00:45:24 -0300

    UtopianTree solution

--- UtopianTree/UtopianTree/Program.cs
+++ UtopianTree/UtopianTree/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,39 @@ namespace UtopianTree
 {
     class Program
     {
+        // Complete the utopianTree function below.
+        static int utopianTree(int n)
+        {
+            int height = 1;
+
+            for (int i = 0; i < n; ++i)
+            {
+                if (i % 2 == 0)
+                    height *= 2;
+                else
+                    height++;
+            }
+
+            return height;
+        }
+
         static void Main(string[] args)
         {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                int result = utopianTree(n);
+
+                textWriter.WriteLine(result);
+            }
+
+            textWriter.Flush();
+            textWriter.Close();
         }
     }
 }
