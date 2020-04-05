6b2a5b740ea791484f52c6bbabe9c03691d185f8
Leandro Alves Santos <lealsto82@gmail.com>
2020-04-04 21:31:32 -0300

    Cats and Mouse project created

---
+++ Cat and Mouse/Cat and Mouse/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cat_and_Mouse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
