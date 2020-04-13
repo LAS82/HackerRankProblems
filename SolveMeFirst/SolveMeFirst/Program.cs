e6de7aa9c4657cf6b36b259fa4e1b68e4f348b33
Leandro Alves Santos <lealsto82@gmail.com>
2020-04-13 00:21:58 -0300

    Solve me first solution

--- SolveMeFirst/SolveMeFirst/Program.cs
+++ SolveMeFirst/SolveMeFirst/Program.cs
@@ -8,8 +8,18 @@ namespace SolveMeFirst
 {
     class Program
     {
-        static void Main(string[] args)
+        static int solveMeFirst(int a, int b)
         {
+            // Hint: Type return a+b; below  
+            return a + b;
+        }
+
+        static void Main(String[] args)
+        {
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            int val2 = Convert.ToInt32(Console.ReadLine());
+            int sum = solveMeFirst(val1, val2);
+            Console.WriteLine(sum);
         }
     }
 }
