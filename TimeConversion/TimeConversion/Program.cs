d6d9acc6cb5bf81f3149c390a339dfe1c23d8dfb
Leandro Alves Santos <lealsto82@gmail.com>
2020-04-15 18:35:00 -0300

    Time conversion created

---
+++ TimeConversion/TimeConversion/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeConversion
+{
+    class Program
+    {
+        /*
+        * Complete the timeConversion function below.
+        */
+        static string timeConversion(string s)
+        {
+            /*
+             * Write your code here.
+             */
+
+            return String.Empty;
+        }
+
+        static void Main(string[] args)
+        {
+
+            string s = Console.ReadLine();
+
+            string result = timeConversion(s);
+
+
+            //To run on Visual Studio
+            Console.WriteLine(result);
+
+            /*
+            //To run on HackerRank
+            TextWriter tw = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+            tw.WriteLine(result);
+
+            tw.Flush();
+            tw.Close();
+            */
+
+        }
+    }
+}
