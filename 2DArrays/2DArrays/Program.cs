4d19b87802e0309d75c962ad4b816c56380dd00c
Leandro Alves Santos <lealsto82@gmail.com>
2020-05-02 11:23:42 -0300

    2DArrays created

---
+++ 2DArrays/2DArrays/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DArrays
+{
+    class Program
+    {
+        static int GetMaxHourGlassSum(int[][] arr)
+        {
+            return Int32.MinValue;
+        }
+
+        static void Main(string[] args)
+        {
+            int[][] arr = new int[6][];
+
+            for (int i = 0; i < 6; i++)
+            {
+                arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            }
+
+            Console.WriteLine(GetMaxHourGlassSum(arr));
+        }
+    }
+}
