f6bdde1fda1654d3676add2a0072435065363c20
Leandro Alves Santos <lealsto82@gmail.com>
2020-05-06 23:38:10 -0300

    Migratory Birds created

---
+++ MigratoryBirds/MigratoryBirds/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the migratoryBirds function below.
+    static int migratoryBirds(List<int> arr)
+    {
+
+        return 0;
+
+    }
+
+    static void Main(string[] args)
+    {
+        int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int result = migratoryBirds(arr);
+
+        //To run on Visual Studio
+        Console.WriteLine(result);
+        Console.ReadKey();
+
+        /*
+        //To run on hacker rank
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+        */
+    }
+}
