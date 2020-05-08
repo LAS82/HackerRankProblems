dd780e09df016ff94e08b4545a0887e293016a3e
Leandro Alves Santos <lealsto82@gmail.com>
2020-05-08 03:50:19 -0300

    Marcs Cakewalk Created

---
+++ MarcsCakewalk/MarcsCakewalk/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the marcsCakewalk function below.
+    static long marcsCakewalk(int[] calorie)
+    {
+        return 0;
+
+    }
+
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] calorie = Array.ConvertAll(Console.ReadLine().Split(' '), calorieTemp => Convert.ToInt32(calorieTemp));
+        long result = marcsCakewalk(calorie);
+
+
+        //to run on visual studio
+        Console.WriteLine(result);
+        Console.ReadKey();
+
+        /*
+        //To run on hacker rank
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+        */
+    }
+}
