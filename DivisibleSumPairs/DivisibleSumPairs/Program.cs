b80aaf1adfe2058cdf68a2eca88a1d415f4a97be
Leandro Alves Santos <lealsto82@gmail.com>
2020-05-15 20:35:57 -0300

    DivisibleSumPairs solution

--- DivisibleSumPairs/DivisibleSumPairs/Program.cs
+++ DivisibleSumPairs/DivisibleSumPairs/Program.cs
@@ -19,7 +19,18 @@ class Solution
     static int divisibleSumPairs(int n, int k, int[] ar)
     {
 
-        return 0;
+        int count = 0;
+
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = i + 1; j < n; ++j)
+            {
+                if ((ar[i] + ar[j]) % k == 0)
+                    count++;
+            }
+        }
+
+        return count;
 
     }
 
