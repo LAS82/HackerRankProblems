99d90cb14cb113192ee0c0ccbcae05055e53d657
Leandro Alves Santos <lealsto82@gmail.com>
2020-08-11 01:01:54 -0300

    Solution

--- AngryProfessor/AngryProfessor/Program.cs
+++ AngryProfessor/AngryProfessor/Program.cs
@@ -1,15 +1,61 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
 using System.Text;
-using System.Threading.Tasks;
+using System;
 
-namespace AngryProfessor
+class Solution
 {
-    class Program
+
+    // Complete the angryProfessor function below.
+    static string angryProfessor(int k, int[] a)
     {
-        static void Main(string[] args)
+
+        int arrived = 0;
+
+        for (int i = 0; i < a.Length; ++i)
         {
+            if (a[i] <= 0)
+                arrived++;
+
+            if (arrived == k)
+                return "NO";
         }
+
+        return "YES";
+
+    }
+
+    static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            string[] nk = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(nk[0]);
+
+            int k = Convert.ToInt32(nk[1]);
+
+            int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), aTemp => Convert.ToInt32(aTemp));
+            string result = angryProfessor(k, a);
+
+            //textWriter.WriteLine(result);
+            Console.WriteLine(result);
+        }
+
+        //textWriter.Flush();
+        //textWriter.Close();
     }
 }
