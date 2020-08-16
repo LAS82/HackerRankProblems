f95286c1784521fc7e05202ea7e8a5e9500dc5f0
Leandro Alves Santos <lealsto82@gmail.com>
2020-08-15 23:43:33 -0300

    jumpingOnClouds Project created

---
+++ JumpingOnTheClouds/JumpingOnTheClouds/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the jumpingOnClouds function below.
+    static int jumpingOnClouds(int[] c, int k)
+    {
+
+
+    }
+
+    static void Main(string[] args)
+    {
+        
+
+        string[] nk = Console.ReadLine().Split(' ');
+
+        int n = Convert.ToInt32(nk[0]);
+
+        int k = Convert.ToInt32(nk[1]);
+
+        int[] c = Array.ConvertAll(Console.ReadLine().Split(' '), cTemp => Convert.ToInt32(cTemp));
+
+        int result = jumpingOnClouds(c, k);
+
+        //To run on visual studio
+        Console.WriteLine(result);
+
+        /*
+        //To run on hackerrank
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+        */
+    }
+}
